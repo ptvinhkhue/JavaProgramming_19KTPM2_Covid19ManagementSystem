8c5f3a502ce7032369b2f2769795da64c3a307af
ptvinhkhue <ptvinhkhue2001@gmail.com>
2021-11-19 03:16:45 +0700

    Setup basic log

--- logfourj/src/main/java/com/cookies/logfourj/Log4J.java
+++ logfourj/src/main/java/com/cookies/logfourj/Log4J.java
@@ -3,11 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.cookies.logfourj;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  *
  * @author ptvin
  */
 public class Log4J {
-    
+    private static final Logger logger = LogManager.getLogger(Log4J.class);
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello, user!" );
+        logger.trace("We've just greeted the user!");
+        logger.debug("We've just greeted the user!");
+        logger.info("We've just greeted the user!");
+        logger.warn("We've just greeted the user!");
+        logger.error("We've just greeted the user!");
+        logger.fatal("We've just greeted the user!");
+    }
 }
