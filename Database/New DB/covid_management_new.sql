5565e09c7b4c44dcf0cd3489db367c5a87ee9eed
Tung Tran <56991132+C2H4Br2@users.noreply.github.com>
2022-01-09 23:52:05 +0700

    Reupload new DB

--- Database/New DB/covid_management_new.sql
+++ Database/New DB/covid_management_new.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `covid_management` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `covid_management`;
 -- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: covid_management
@@ -500,4 +502,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-09 23:22:19
+-- Dump completed on 2022-01-09 23:51:48
